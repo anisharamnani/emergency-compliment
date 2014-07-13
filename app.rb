6fccf32333cc7edde48ee01830088b5840316adc
anisharamnani <anisha.ramnani@gmail.com>
2014-07-13 18:06:10 -0400

    update and style view to display compliment

--- app.rb
+++ app.rb
@@ -1,2 +1,30 @@
-require './app'
-run EmergencyCompliment::App.new
+require 'sinatra/base'
+require './lib/compliment'
+
+module EmergencyCompliment
+  class App < Sinatra::Base
+    enable :sessions
+    set :session_secret, 'super secret'
+
+    get '/' do
+      @compliment = Compliment.new
+      if session[:last_message]
+        while @compliment.message == session[:last_message]
+          @compliment.message = @compliment.select_message
+        end
+      end
+
+      if session[:last_image]
+        while @compliment.image == session[:last_image]
+          @compliment.image = @compliment.select_image
+        end
+      end
+
+      session[:last_message] = @compliment.message
+      session[:last_image] = @compliment.image
+      
+      haml :compliment
+    end
+
+  end
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require './app'
+run EmergencyCompliment::App.new
